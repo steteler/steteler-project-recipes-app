d1c0386650b8867e55bfa3ade49f2d987b85661e
kikuti <joaovictorkikuti@gmail.com>
2023-08-23 00:42:14 -0300

    adicionado browser router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom/BrowserRouter';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 import './index.css';
